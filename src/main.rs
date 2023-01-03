8d0bf199e4f24078ec220129c81de9ccf31892a5
dan <dan@pop-os.localdomain>
2023-01-03 17:31:17 -0600

    enemy start

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,25 @@ fn main() -> rltk::BError {
 
     let map: GameMap = new_map_and_corridoors();
     let (player_x, player_y) = map.rooms[0].center();
+
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        gs.ecs
+            .create_entity()
+            .with(Position { x, y })
+            .with(Renderable {
+                glyph: rltk::to_cp437('b'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed {
+                visible_tiles: Vec::new(),
+                range: 8,
+                dirty: true,
+            })
+            .build();
+    }
+
     gs.ecs.insert(map);
 
     gs.ecs
