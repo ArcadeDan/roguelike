a06a1a6314d00cac135d7dd84c6543d666a66b8b
dan <dan@pop-os.localdomain>
2023-01-05 18:02:46 -0600

    enemy collision work

--- src/NPC/movement.rs
+++ src/NPC/movement.rs
@@ -17,7 +17,7 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
 
     for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map.tiles[destination_idx] != TileType::Wall {
+        if !map.blocked[destination_idx]  {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
 
